0759b786446da6ab3170c6fc2dc9f4022ddf887e
dchnk <seroxe2@yandex.ru>
2024-04-19 08:07:36 +0700

    feat

--- script.js
+++ script.js
@@ -12,17 +12,19 @@ const config = {
     create: create,
     update: update
   },
-  // physics: {
-  //   default: 'arcade',
-  //   arcade: {
-  //     gravity: { y: 300 },
-  //     debug: false
-  //   }
-  // },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
+    }
+  },
 }
 
 // const game = new Phaser.Game(config);
 new Phaser.Game(config);
+
+let player, platforms, cursors;
 
 function preload() {
   this.load.image('map', './src/img/map.png');
@@ -34,7 +36,71 @@ function preload() {
 
 function create() {
   this.add.image(-100, -100, 'map').setOrigin(0, 0);
+  console.log(this)
+
+  platforms = this.physics.add.staticGroup();
+
+  player = this.physics.add.sprite(500, 500, 'cook');
+  // player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('cook', { start: 15, end: 20 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'left-stop',
+    frames: [ { key: 'cook', frame: 14 } ],
+    frameRate: 20,
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('cook', { start: 22, end: 27 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'top',
+    frames: this.anims.generateFrameNumbers('cook', { start: 1, end: 20 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'bottom',
+    frames: this.anims.generateFrameNumbers('cook', { start: 21, end: 27 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+
+  cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
+
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+
+    player.anims.play('left', true);
+  }
+  else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+
+    player.anims.play('right', true);
+  }
+  // else {
+  //   player.setVelocityX(0);
+
+  //   player.anims.play('turn');
+  // }
+
+  if (cursors.up.isDown) {
+    player.setVelocityY(-330);
+  }
 }
